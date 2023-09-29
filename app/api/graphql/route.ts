18e64f71347993c4f4e55d141d1a9e2b98d174d4
nauroz <alinauroze@hotmail.com>
2023-09-29 22:29:42 +0500

    graphql

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,19 @@
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { ApolloServer } from '@apollo/server';
+import { NextRequest } from 'next/server';
+import resolvers from '@/server/graphql/resolvers/queries';
+import typeDefs from '@/server/graphql/typedefs';
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+});
+const handler = startServerAndCreateNextHandler<NextRequest>(server);
+
+export async function GET(request: NextRequest) {
+  return handler(request);
+}
+
+export async function POST(request: NextRequest) {
+  return handler(request);
+}
