6155ccbd6e9080e0ee471feb483ac50004a2ac0b
nauroz <alinauroze@hotmail.com>
2023-10-10 01:00:55 +0500

    send profile

--- bot/utils/send.ts
+++ bot/utils/send.ts
@@ -1,3 +1,5 @@
+import prisma from '../../prisma/prisma';
+
 const TelegramBot = require('node-telegram-bot-api');
 
 interface ISendProfile {
@@ -5,4 +7,43 @@ interface ISendProfile {
   chatId: number;
 }
 
-export const sendProfile = ({}: ISendProfile) => {};
+function replaceAll(inputString: string, search: string, replacement: string) {
+  const regex = new RegExp(search, 'g');
+  return inputString.replace(regex, replacement);
+}
+
+export const sendProfile = async ({ bot, chatId }: ISendProfile) => {
+  const user = await prisma.user.findUnique({ where: { chatId } });
+
+  let message = `This is your profile\n`;
+  if (user?.name) {
+    message += `\n*Name* ${replaceAll(
+      replaceAll(user.name, '_', '-'),
+      '_',
+      '-'
+    )}`;
+  }
+  if (user?.email) {
+    message += `\n*Email:* ${replaceAll(
+      replaceAll(user.email, '_', '-'),
+      '_',
+      '-'
+    )}`;
+  }
+  if (user?.experience) {
+    message += `\n*Expereince:* ${user?.experience}`;
+  }
+  if (user?.gender) {
+    message += `\n*Gender*: ${user.gender}`;
+  }
+  if (user?.race) {
+    message += `\n*Race*: ${user.race}`;
+  }
+  if (user?.citizenship) {
+    message += `\n*Citizenship:* ${user.citizenship}`;
+  }
+
+  bot.sendMessage(chatId, message, {
+    parse_mode: 'markdown',
+  });
+};
