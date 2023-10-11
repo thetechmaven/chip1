6af4f282be74c98a52821a45396a6084283760fb
nauroz <alinauroze@hotmail.com>
2023-10-11 17:13:50 +0500

    load users

--- components/Dashboard/Users/index.tsx
+++ components/Dashboard/Users/index.tsx
@@ -1,10 +1,48 @@
+'use client';
+
 import React from 'react';
 import Layout from '../Layout';
+import { gql, useQuery } from 'urql';
+import Loading from '@/components/Elements/Loading';
+
+const GET_USERS = gql`
+  query Users {
+    users {
+      categoryPreference
+      chatId
+      cover
+      citizenship
+      dob
+      experience
+      gender
+      group
+      hp
+      id
+      isAdmin
+      isStaff
+      locationPreference
+      name
+      phone
+      qualification
+      race
+      typePreference
+      username
+    }
+  }
+`;
 
 function Users() {
+  const [{ fetching, data }] = useQuery({ query: GET_USERS });
+
   return (
     <Layout heading="Users">
-      <p>Users here</p>
+      {fetching ? (
+        <div className="flex justify-center items-center min-h-screen">
+          <Loading />
+        </div>
+      ) : (
+        <div></div>
+      )}
     </Layout>
   );
 }
