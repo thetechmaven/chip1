f398f4b753b0ea01304b82ef9434b8be34995be9
nauroz <alinauroze@hotmail.com>
2025-01-26 02:12:12 +0500

    fix: tags parsing

--- bot/utils/profile.ts
+++ bot/utils/profile.ts
@@ -18,7 +18,11 @@ ${JSON.stringify(user)}
 
 Extract tags based on user location, niche, and packages. If any information is missing, ignore it. You can generate upto 25 tags which can help in searching creators. If there is less information, you can add some related tags yourself to that this creator can be found by brands. Output as array of strings which can be parsed by JSON.parse and dont add any extra text:
 For example: ["min_package_price:<min package price if available>", "max_package_price:<max package price if available>", "<tag1>", "<tag2>", ...]`,
-      true
+      true,
+      [],
+      {
+        jsonResponse: true,
+      }
     );
     const tags = JSON.parse(tagsString);
     tags.push(`location:${user.location}`);
