498f0fc5915876797e7352d045bad30e5e1bf3e7
nauroz <alinauroze@hotmail.com>
2025-01-31 04:19:24 +0500

    group

--- bot/handlers/groupHandlers.ts
+++ bot/handlers/groupHandlers.ts
@@ -1,11 +1,37 @@
 const TelegramBot = require('node-telegram-bot-api');
 import type * as TelegramBotTypes from 'node-telegram-bot-api';
-import { isCreatorGroup } from '../utils/groupHelpers';
+import {
+  getChatId,
+  getKnowledgeBase,
+  initGroup,
+  isCreatorGroup,
+  sendRequestToEliza,
+} from '../utils/groupHelpers';
 
 export const groupHandler = async (
   bot: typeof TelegramBot,
   message: TelegramBotTypes.Message
 ) => {
-  const isCreator = await isCreatorGroup(message);
-  console.log('Is creator', isCreator);
+  const { isCreator, isNewGroup } = await isCreatorGroup(message);
+  if (isNewGroup) {
+    await initGroup(message);
+    bot.sendMessage(getChatId(message), 'Group initialized');
+  } else {
+    if (isCreator) {
+      bot.sendMessage(
+        getChatId(message),
+        'Temp Message: will be removed because Chip dont reply to creators, only replies to buyers'
+      );
+    } else {
+      const knowledgebase = await getKnowledgeBase(getChatId(message));
+      const data = await sendRequestToEliza(
+        getChatId(message),
+        (message.text as string) +
+          `\n\nNote: In most cases when client refers "You" that means the content creator.\n\nUse the following knowledgebase to help the user if required: \n${knowledgebase} \n`
+      );
+      bot.sendMessage(getChatId(message), data[0].text, {
+        parse_mode: 'Markdown',
+      });
+    }
+  }
 };
--- bot/utils/groupHelpers.ts
+++ bot/utils/groupHelpers.ts
@@ -11,11 +11,99 @@ export const getChatId = (message: TelegramBotTypes.Message) => {
   return message.chat.id;
 };
 
-export const isCreatorGroup = async (message: TelegramBotTypes.Message) => {
+export const sendRequestToEliza = async (chatId: number, prompt: string) => {
+  const response = await fetch(
+    'http://localhost:3000/d9524aeb-bd0d-010a-bc17-876c2543336a/message',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text: prompt,
+        user: 'u' + chatId,
+      }),
+    }
+  );
+  const data = await response.json();
+  return data;
+};
+
+const creators: any = {};
+export const getKnowledgeBase = async (chatId: number) => {
+  let creator = creators[chatId];
+  if (!creator) {
+    const user = await prisma.group.findFirst({
+      where: { groupChatId: chatId },
+      include: {
+        creator: {
+          include: {
+            packages: true,
+          },
+        },
+      },
+    });
+    creator = user?.creator;
+    creators[chatId] = creator;
+  }
+  if (creator) {
+    const packages = creator.packages;
+    const knowledgebase =
+      `Content creator details: \n` +
+      `${Object.keys(creator)
+        .map((key: any) => `${key}: ${(creator as any)[key]}`)
+        .join('\n')}\n` +
+      `Content creator packages: \n` +
+      `${packages
+        .map(
+          (p: any) =>
+            `${p.name}(Price: ${p.price} USD${
+              p.negotitationLimit ? ` with a ± ${p.negotitationLimit}%` : ``
+            }): ${p.description || 'No Description'}`
+        )
+        .join('\n')}\n`;
+    return knowledgebase;
+  }
+};
+
+export const initGroup = async (message: TelegramBotTypes.Message) => {
+  const document = await prisma.user.findUnique({
+    where: { chatId: message.from?.id },
+    include: {
+      packages: true,
+    },
+  });
+  if (document) {
+    const { packages, ...user } = document;
+    const prompt =
+      `Hello Chip!` +
+      `Im a content creator. Here are my details: \n` +
+      `${Object.keys(user)
+        .map((key: any) => `${key}: ${(user as any)[key]}`)
+        .join('\n')}\n` +
+      `My packages are: \n` +
+      `${packages
+        .map(
+          (p: any) =>
+            `${p.name}(Price: ${p.price} USD${
+              p.negotitationLimit ? ` with a ± ${p.negotitationLimit}%` : ``
+            }): ${p.description || 'No Description'}`
+        )
+        .join('\n')}\n` +
+      `Now, your role will be inform my clients about my package and negotiate with them. Dont tell them negotiation limit but try to make a good deal. Dont let the client go!`;
+    console.log('Prompt: ', prompt);
+    sendRequestToEliza(getChatId(message), prompt);
+  }
+};
+
+export const isCreatorGroup = async (
+  message: TelegramBotTypes.Message
+): Promise<{ isCreator: boolean; isNewGroup?: boolean }> => {
   const chatId = getChatId(message);
   if (chatId in groupCreatorMap) {
-    return groupCreatorMap[chatId].creator === message.from?.id;
+    return { isCreator: groupCreatorMap[chatId].creator === message.from?.id };
   } else {
+    let isNewGroup = false;
     let group = await prisma.group.findUnique({
       where: { groupChatId: chatId },
       include: {
@@ -27,8 +115,9 @@ export const isCreatorGroup = async (message: TelegramBotTypes.Message) => {
         where: { chatId: message.from?.id },
       });
       if (!user) {
-        return false;
+        return { isCreator: false };
       }
+      isNewGroup = true;
       group = await prisma.group.create({
         data: {
           groupChatId: chatId,
@@ -41,7 +130,6 @@ export const isCreatorGroup = async (message: TelegramBotTypes.Message) => {
       });
     }
     groupCreatorMap[chatId] = { creator: group.creator.chatId };
-    console.log('Group creator map', groupCreatorMap[chatId]);
-    return group.creator.chatId === message.from?.id;
+    return { isCreator: group.creator.chatId === message.from?.id, isNewGroup };
   }
 };
