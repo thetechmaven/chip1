c2e351a38e5e4094189a92e46bdab91ccc8fef67
nauroz <alinauroze@hotmail.com>
2025-02-14 00:09:11 +0500

    sub text alignment

--- bot/utils/imageGeneration.ts
+++ bot/utils/imageGeneration.ts
@@ -1,9 +1,11 @@
+import prisma from '../../prisma/prisma';
+
 const axios = require('axios');
 const fs = require('fs');
 const sharp = require('sharp');
 const { createCanvas, registerFont } = require('canvas');
 const path = require('path');
-/*
+
 export const generateImage = async (bot: any, msg: any) => {
   const TOKEN = process.env.BOT_TOKEN;
   const chatId = msg.chat.id;
@@ -36,11 +38,17 @@ export const generateImage = async (bot: any, msg: any) => {
     response.data.pipe(writer);
     writer.on('finish', async () => {
       console.log('Profile picture downloaded:', profilePicPath);
-
+      const user = await prisma.user.findUnique({
+        where: { chatId: msg.chat.id },
+      });
+      if (!user) {
+        return;
+      }
       await processImage(
         profilePicPath,
         finalImagePath,
-        msg.from.first_name + ' ' + (msg.from.last_name || '')
+        msg.from.first_name + ' ' + (msg.from.last_name || ''),
+        user.niche + '    ' + user.contentStyle
       );
 
       if (fs.existsSync(finalImagePath)) {
@@ -61,75 +69,9 @@ export const generateImage = async (bot: any, msg: any) => {
 async function processImage(
   profilePicPath: any,
   outputImagePath: any,
-  text: string
+  text: string,
+  text2: string
 ) {
-  try {
-    console.log('Processing image...');
-
-    // Load the profile image
-    const profileImage = await sharp(profilePicPath)
-      .resize(273, 273) // Resize profile picture
-      .toBuffer();
-
-    // Load the background image
-    const background = sharp('./template.jpg');
-    const metadata = await background.metadata();
-
-    const fontPath = './IGOR-BoldItalic.ttf';
-    const fontSize = 46; // Base font size, adjust as needed
-
-    if (fs.existsSync(fontPath)) {
-      registerFont(fontPath, { family: 'IGORE-BoldItalic' });
-      console.log('Font loaded:', fontPath);
-    } else {
-      console.error('Font not found:', fontPath);
-    }
-    const tempCanvas = createCanvas(500, 200);
-    const tempCtx = tempCanvas.getContext('2d');
-    tempCtx.font = `${fontSize}px IGORE-BoldItalic`;
-
-    // Measure text dimensions
-
-    const textWidth = Math.ceil(tempCtx.measureText(text).width) + 20; // Add padding
-    const textHeight = fontSize; // Use exact font size (no extra padding)
-
-    // Create a real canvas with correct dimensions for the SVG
-    const canvas = createCanvas(textWidth, textHeight, 'svg');
-    const ctx = canvas.getContext('2d');
-    ctx.font = `${fontSize}px CustomFont, Arial`;
-    ctx.fillStyle = '#dd2173';
-    ctx.textBaseline = 'top';
-    ctx.fillText(text, 0, 0);
-
-    const canvas2 = createCanvas(textWidth, textHeight, 'svg');
-    const ctx2 = canvas2.getContext('2d');
-    ctx2.font = `${fontSize}px CustomFont, Arial`;
-    ctx2.fillStyle = '#dd2173';
-    ctx2.textBaseline = 'top';
-    ctx2.fillText("Streaming        Gaming", 0, 0);
-
-    // Get the SVG buffer from the canvas
-    const svgBuffer = Buffer.from(canvas.toBuffer());
-    const nicheSVG = Buffer.from(canvas2.toBuffer());
-
-    // Composite the profile image onto the background
-    await background
-      .composite([
-        { input: profileImage, top: 255, left: 195 },
-        { input: Buffer.from(svgBuffer as any), top: 570, left: 224 }, // Text position
-        { input: Buffer.from(nicheSVG as any), top: 574, left: 106 }, // Text position
-        // { input: Buffer.from(nicheSVG), top: 654, left: 390 } // Text position
-      ]) // Position the profile image
-      .toFile(outputImagePath);
-
-    console.log('Image processing completed:', outputImagePath);
-  } catch (error) {
-    console.error('Error processing image:', error);
-  }
-}
-*/
-
-async function processImage(profilePicPath, outputImagePath, text, text2) {
   try {
     console.log('Processing image...');
 
@@ -175,16 +117,16 @@ async function processImage(profilePicPath, outputImagePath, text, text2) {
       ctx.font = `${fontSizeSub}px IGOR-BoldItalic`;
       const textWidth = ctx.measureText(text2).width;
 
-      if (textWidth <= 360) {
+      if (textWidth <= 320) {
         break;
       }
       fontSizeSub--;
     } while (fontSizeSub > 10);
-
+    const subTextHeight = ctx.measureText(text2).height;
     ctx2.font = `${fontSizeSub}px IGORE-BoldItalic`;
     ctx2.fillStyle = '#dd2173';
     ctx2.textBaseline = 'top';
-    ctx2.fillText(text2, 0, 0);
+    ctx2.fillText(text2, 0, (textHeight - fontSizeSub) / 2);
 
     // Get the SVG buffer from the canvas
     const svgBuffer = Buffer.from(canvas.toBuffer());
@@ -194,8 +136,8 @@ async function processImage(profilePicPath, outputImagePath, text, text2) {
     await background
       .composite([
         { input: profileImage, top: 255, left: 195 },
-        { input: Buffer.from(svgBuffer), top: 570, left: 116 }, // Text position
-        { input: Buffer.from(nicheSVG), top: 634, left: 116 }, // Text position
+        { input: Buffer.from(svgBuffer as any), top: 570, left: 116 }, // Text position
+        { input: Buffer.from(nicheSVG as any), top: 634, left: 116 }, // Text position
         // { input: Buffer.from(nicheSVG), top: 654, left: 390 } // Text position
       ]) // Position the profile image
       .toFile(outputImagePath);
@@ -205,10 +147,3 @@ async function processImage(profilePicPath, outputImagePath, text, text2) {
     console.error('Error processing image:', error);
   }
 }
-
-processImage(
-  path.join(__dirname, '../../profile.jpeg'),
-  path.join(__dirname, '../../output.jpg'),
-  'Ali Nauroze',
-  'Gaming      Streaming'
-);
