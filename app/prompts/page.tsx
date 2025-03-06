cfc2180b38b9961005ae9f1c22ce94d116e70946
nauroz <alinauroze@hotmail.com>
2025-03-06 23:35:16 +0500

    load prompts

--- app/prompts/page.tsx
+++ app/prompts/page.tsx
@@ -1,15 +1,32 @@
 import Prompts from '@/components/Dashboard/Prompts';
+import prisma from '@/prisma/prisma';
 import fs from 'fs';
 import path from 'path';
 
-function getPrompts() {
+async function getPrompts() {
+  const prompts = await prisma.prompt.findMany();
+  const data: { [key: string]: string } = {};
+  prompts.forEach((prompt) => {
+    data[prompt.key] = prompt.value;
+  });
+
   const filePath = path.join(process.cwd(), 'prompts.json');
   const fileContents = fs.readFileSync(filePath, 'utf8');
-  const prompts = JSON.parse(fileContents);
-  return prompts;
+  const defaultPromptsObject = JSON.parse(fileContents);
+  const defaultPrompts = Object.keys(defaultPromptsObject).map((key) => {
+    return { key, value: defaultPromptsObject[key] };
+  });
+
+  defaultPrompts.forEach((prompt: { key: string; value: string }) => {
+    if (!data[prompt.key]) {
+      data[prompt.key] = '';
+    }
+  });
+
+  return data;
 }
 
-export default function Page() {
-  const prompts = getPrompts();
+export default async function Page() {
+  const prompts = await getPrompts();
   return <Prompts prompts={prompts} />;
 }
