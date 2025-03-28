fa28621c39610d5e6577267ac104020f86a63978
nauroz <alinauroze@hotmail.com>
2025-03-28 05:40:29 +0500

    description

--- components/Dashboard/Prompts/promptDetails.ts
+++ components/Dashboard/Prompts/promptDetails.ts
@@ -27,7 +27,10 @@ contentStyle
   getCommandAndData: `{commandText} {message}. The output should be object with command field`,
   sendPackage: `{message}: used to send package to user. **Might be replaced soon**`,
   updatePackage: `{currentPackage}, {responseFormat}, {messageText}.  It should return one or more of these fields: name, description, price, negotiationLimit`,
-  group_userPrompt: `{creatorDetails}, {packagesDetails}. Called when  a query is received from a brand in a group`,
+  group_userPrompt: `{creatorDetails}, {packagesDetails}. Used as user prompt when  a query is received from a brand in a group`,
+  group_systemPrompt: `
+    This is system prompt for group communication. 
+  `,
 };
 
 export default promptDetails;
